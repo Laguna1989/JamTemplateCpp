71d6e2b85f26ab56e52e6b89a4d5e8dc3c7cbe1a
Simon Weis <simon.weis@fau.de>
2020-11-22 16:39:11 +0100

    Add first base classes for sdl

---
+++ src/JamTemplate/sdl/color_base.hpp
@@ -0,0 +1,49 @@
+﻿#ifndef COLOR_BASE_GUARD_HPP
+#define COLOR_BASE_GUARD_HPP
+
+#include <SDL.h>
+#include <cstdint>
+
+namespace jt {
+
+class color final {
+public:
+    color()
+        : m_col { 0, 0, 0, 255 }
+    {
+    }
+    color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 255)
+        : m_col { r, g, b, a }
+    {
+    }
+
+    color(SDL_Color const& v)
+        : m_col { v }
+    {
+    }
+
+    ~color() = default;
+    color(jt::color const&) = default;
+    color(jt::color&&) = default;
+
+    color& operator=(jt::color const&) = default;
+    color& operator=(jt::color&&) = default;
+
+    operator SDL_Color() const { return m_col; }
+
+    SDL_Color m_col;
+
+    std::uint8_t r() const { return m_col.r; };
+    std::uint8_t g() const { return m_col.g; };
+    std::uint8_t b() const { return m_col.b; };
+    std::uint8_t a() const { return m_col.a; };
+
+    std::uint8_t& r() { return m_col.r; };
+    std::uint8_t& g() { return m_col.g; };
+    std::uint8_t& b() { return m_col.b; };
+    std::uint8_t& a() { return m_col.a; };
+};
+
+} // namespace jt
+
+#endif
---
+++ src/JamTemplate/sdl/vector_base.hpp
@@ -0,0 +1,94 @@
+#ifndef VECTOR_BASE_GUARD_HPP
+#define VECTOR_BASE_GUARD_HPP
+
+#include <SDL.h>
+
+namespace jt {
+
+class vector2 final {
+public:
+    vector2()
+        : m_x { 0 }
+        , m_y { 0 }
+    {
+    }
+    vector2(float x, float y)
+        : m_x { x }
+        , m_y { y }
+    {
+    }
+
+    vector2(SDL_Point const& v)
+        : m_x { static_cast<float>(v.x) }
+        , m_y { static_cast<float>(v.y) }
+    {
+    }
+
+    ~vector2() = default;
+    vector2(jt::vector2 const&) = default;
+    vector2(jt::vector2&&) = default;
+
+    vector2& operator=(jt::vector2 const&) = default;
+    vector2& operator=(jt::vector2&&) = default;
+
+    operator SDL_Point() const
+    {
+        return SDL_Point { static_cast<int>(m_x), static_cast<int>(m_y) };
+    }
+
+    float x() const { return m_x; };
+    float y() const { return m_y; };
+
+    float& y() { return m_y; }
+    float& x() { return m_x; }
+
+private:
+    float m_x;
+    float m_y;
+};
+
+class vector2u final {
+public:
+    vector2u()
+        : m_x { 0U }
+        , m_y { 0U }
+    {
+    }
+    vector2u(unsigned int x, unsigned int y)
+        : m_x { x }
+        , m_y { y }
+    {
+    }
+
+    vector2u(SDL_Point const& v)
+        : m_x { static_cast<unsigned int>(v.x) }
+        , m_y { static_cast<unsigned int>(v.y) }
+    {
+    }
+
+    ~vector2u() = default;
+    vector2u(jt::vector2u const&) = default;
+    vector2u(jt::vector2u&&) = default;
+
+    jt::vector2u& operator=(jt::vector2u const&) = default;
+    jt::vector2u& operator=(jt::vector2u&&) = default;
+
+    operator SDL_Point() const
+    {
+        return SDL_Point { static_cast<int>(m_x), static_cast<int>(m_y) };
+    }
+
+    unsigned int x() const { return m_x; };
+    unsigned int y() const { return m_y; };
+
+    unsigned int& x() { return m_x; }
+    unsigned int& y() { return m_y; }
+
+private:
+    unsigned int m_x;
+    unsigned int m_y;
+};
+
+} // namespace jt
+
+#endif
