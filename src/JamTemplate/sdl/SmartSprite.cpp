621b7e71fc0d1ec69d6e15de44adbd9042b8a653
Simon Weis <laguna.ws@gmail.com>
2020-12-02 15:36:42 +0100

    Split sdl SmartSprite

---
+++ src/JamTemplate/sdl/SmartSprite.cpp
@@ -0,0 +1,111 @@
+﻿#include "SmartSprite.hpp"
+#include "MathHelper.hpp"
+#include "SDLHelper.hpp"
+#include "TextureManager.hpp"
+#include "rendertarget.hpp"
+
+namespace jt {
+
+void SmartSprite::loadSprite(std::string const& fileName)
+{
+    m_text = TextureManager::get(fileName);
+    int w { 0 };
+    int h { 0 };
+    SDL_QueryTexture(m_text.get(), NULL, NULL, &w, &h); // get the width and height of the texture
+    m_sourceRect = jt::recti { 0, 0, w, h };
+
+    m_textFlash = TextureManager::get(TextureManager::getFlashName(fileName));
+}
+
+void SmartSprite::loadSprite(std::string const& fileName, jt::recti const& rect)
+{
+    m_text = TextureManager::get(fileName);
+    int w { 0 };
+    int h { 0 };
+    SDL_QueryTexture(m_text.get(), NULL, NULL, &w, &h); // get the width and height of the texture
+    m_sourceRect = jt::recti { rect };
+
+    m_textFlash = TextureManager::get(TextureManager::getFlashName(fileName));
+}
+
+void SmartSprite::setPosition(jt::vector2 const& pos) { m_position = pos; }
+const jt::vector2 SmartSprite::getPosition() const { return m_position; }
+
+void SmartSprite::setColor(jt::color const& col) { m_color = col; }
+const jt::color SmartSprite::getColor() const { return m_color; }
+
+void SmartSprite::setFlashColor(jt::color const& col) { m_colorFlash = col; }
+const jt::color SmartSprite::getFlashColor() const { return m_colorFlash; }
+
+//  sf::Transform const getTransform() const  { return m_sprite.getTransform(); }
+
+jt::rect const SmartSprite::getGlobalBounds() const { return jt::rect {}; }
+jt::rect const SmartSprite::getLocalBounds() const { return jt::rect {}; }
+
+void SmartSprite::setScale(jt::vector2 const& scale) { m_scale = scale; }
+const jt::vector2 SmartSprite::getScale() const { return m_scale; }
+
+void SmartSprite::setOrigin(jt::vector2 const& origin) { m_origin = origin; }
+jt::vector2 const SmartSprite::getOrigin() const { return m_origin; }
+
+void SmartSprite::doUpdate(float /*elapsed*/) { }
+
+void SmartSprite::doDraw(std::shared_ptr<jt::renderTarget> const sptr) const
+{
+    SDL_Rect sourceRect = getSourceRect();
+    SDL_Rect destRect = getDestRect();
+    auto flip = jt::getFlipFromScale(m_scale);
+    SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
+    SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
+    setSDLColor(m_color);
+    SDL_RenderCopyEx(sptr.get(), m_text.get(), &sourceRect, &destRect, getRotation(), &p, flip);
+}
+
+void SmartSprite::doDrawShadow(std::shared_ptr<jt::renderTarget> const sptr) const
+{
+    SDL_Rect sourceRect = getSourceRect();
+    SDL_Rect destRect = getDestRect(getShadowOffset());
+    auto flip = jt::getFlipFromScale(m_scale);
+    SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
+    SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
+    setSDLColor(getShadowColor());
+    SDL_RenderCopyEx(sptr.get(), m_text.get(), &sourceRect, &destRect, getRotation(), &p, flip);
+}
+
+void SmartSprite::doDrawFlash(std::shared_ptr<jt::renderTarget> const sptr) const
+{
+    SDL_Rect sourceRect = getSourceRect();
+    SDL_Rect destRect = getDestRect();
+    auto flip = jt::getFlipFromScale(m_scale);
+    SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
+    SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
+    SDL_SetTextureColorMod(
+        m_textFlash.get(), getFlashColor().r(), getFlashColor().g(), getFlashColor().b());
+    SDL_SetTextureAlphaMod(m_textFlash.get(), getFlashColor().a());
+    SDL_RenderCopyEx(
+        sptr.get(), m_textFlash.get(), &sourceRect, &destRect, getRotation(), &p, flip);
+}
+
+void SmartSprite::doRotate(float /*rot*/) { }
+
+SDL_Rect SmartSprite::getDestRect(jt::vector2 const& positionOffset) const
+{
+    auto const pos = m_position + getShakeOffset() + getOffset() + getCamOffset() + positionOffset;
+    SDL_Rect destRect { static_cast<int>(pos.x()), static_cast<int>(pos.y()),
+        static_cast<int>(m_sourceRect.width() * fabs(m_scale.x())),
+        static_cast<int>(m_sourceRect.height() * fabs(m_scale.y())) };
+    return destRect;
+}
+SDL_Rect SmartSprite::getSourceRect() const
+{
+    return SDL_Rect { m_sourceRect.left(), m_sourceRect.top(), m_sourceRect.width(),
+        m_sourceRect.height() };
+}
+
+void SmartSprite::setSDLColor(jt::color const& col) const
+{
+    SDL_SetTextureColorMod(m_text.get(), col.r(), col.g(), col.b());
+    SDL_SetTextureAlphaMod(m_text.get(), col.a());
+}
+
+} // namespace jt
--- src/JamTemplate/sdl/SmartSprite.hpp
+++ src/JamTemplate/sdl/SmartSprite.hpp
@@ -1,10 +1,10 @@
 ﻿#ifndef JAMTEMPLATE_SMARTSPRITE_HPP_INCLUDEGUARD
 #define JAMTEMPLATE_SMARTSPRITE_HPP_INCLUDEGUARD
 
-#include "MathHelper.hpp"
 #include "SmartDrawable.hpp"
-#include "TextureManager.hpp"
+#include "color.hpp"
 #include "rendertarget.hpp"
+#include "vector.hpp"
 #include <SDL.h>
 #include <memory>
 #include <string>
@@ -15,52 +15,28 @@ class SmartSprite : public SmartDrawable {
 public:
     using Sptr = std::shared_ptr<SmartSprite>;
 
-    void loadSprite(std::string const& fileName)
-    {
-        m_text = TextureManager::get(fileName);
-        int w { 0 };
-        int h { 0 };
-        SDL_QueryTexture(
-            m_text.get(), NULL, NULL, &w, &h); // get the width and height of the texture
-        m_sourceRect = jt::recti { 0, 0, w, h };
+    void loadSprite(std::string const& fileName);
+    void loadSprite(std::string const& fileName, jt::recti const& rect);
 
-        m_textFlash = TextureManager::get(TextureManager::getFlashName(fileName));
-    }
+    void setPosition(jt::vector2 const& pos) override;
+    const jt::vector2 getPosition() const override;
 
-    void loadSprite(std::string const& fileName, jt::recti const& rect)
-    {
-        m_text = TextureManager::get(fileName);
-        int w { 0 };
-        int h { 0 };
-        SDL_QueryTexture(
-            m_text.get(), NULL, NULL, &w, &h); // get the width and height of the texture
-        m_sourceRect = jt::recti { rect };
+    void setColor(jt::color const& col) override;
+    const jt::color getColor() const override;
 
-        m_textFlash = TextureManager::get(TextureManager::getFlashName(fileName));
-    }
-
-    void setPosition(jt::vector2 const& pos) override { m_position = pos; }
-
-    const jt::vector2 getPosition() const override { return m_position; }
-
-    void setColor(jt::color const& col) override { m_color = col; }
-    const jt::color getColor() const override { return m_color; }
-
-    void setFlashColor(jt::color const& col) override { m_colorFlash = col; }
-    const jt::color getFlashColor() const override { return m_colorFlash; }
+    void setFlashColor(jt::color const& col) override;
+    const jt::color getFlashColor() const override;
 
     // virtual sf::Transform const getTransform() const override { return m_sprite.getTransform(); }
 
-    virtual jt::rect const getGlobalBounds() const override { return jt::rect {}; }
-    virtual jt::rect const getLocalBounds() const override { return jt::rect {}; }
+    virtual jt::rect const getGlobalBounds() const override;
+    virtual jt::rect const getLocalBounds() const override;
 
-    virtual void setScale(jt::vector2 const& scale) override { m_scale = scale; }
+    virtual void setScale(jt::vector2 const& scale) override;
+    virtual const jt::vector2 getScale() const override;
 
-    virtual const jt::vector2 getScale() const override { return m_scale; }
-
-    virtual void setOrigin(jt::vector2 const& origin) override { m_origin = origin; }
-
-    virtual jt::vector2 const getOrigin() const override { return m_origin; }
+    virtual void setOrigin(jt::vector2 const& origin) override;
+    virtual jt::vector2 const getOrigin() const override;
 
 private:
     mutable std::shared_ptr<SDL_Texture> m_text;
@@ -74,93 +50,19 @@ private:
     mutable std::shared_ptr<SDL_Texture> m_textFlash;
     jt::color m_colorFlash { jt::colors::White };
 
-    void doUpdate(float /*elapsed*/) override
-    {
+    void doUpdate(float /*elapsed*/) override;
 
-        // m_flashSprite.setPosition(pos);
-    }
+    void doDraw(std::shared_ptr<jt::renderTarget> const sptr) const override;
 
-    void doDraw(std::shared_ptr<jt::renderTarget> const sptr) const override
-    {
-        auto const pos = m_position + getShakeOffset() + getOffset() + getCamOffset();
-        SDL_Rect sourceRect { m_sourceRect.left(), m_sourceRect.top(), m_sourceRect.width(),
-            m_sourceRect.height() };
-        float scalex = fabs(m_scale.x());
-        float scaley = fabs(m_scale.y());
-        auto flip = SDL_FLIP_NONE;
-        if (m_scale.x() < 0 && m_scale.y() < 0) {
-            flip = static_cast<SDL_RendererFlip>(SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL);
-        } else if (m_scale.x() < 0 && m_scale.y() >= 0) {
-            flip = SDL_FLIP_HORIZONTAL;
-        } else if (m_scale.x() >= 0 && m_scale.y() < 0) {
-            flip = SDL_FLIP_VERTICAL;
-        }
-        SDL_Rect destRect { static_cast<int>(pos.x()), static_cast<int>(pos.y()),
-            static_cast<int>(m_sourceRect.width() * scalex),
-            static_cast<int>(m_sourceRect.height() * scaley) };
-        SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
-        SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
-        SDL_SetTextureColorMod(m_text.get(), m_color.r(), m_color.g(), m_color.b());
-        SDL_SetTextureAlphaMod(m_text.get(), m_color.a());
-        SDL_RenderCopyEx(sptr.get(), m_text.get(), &sourceRect, &destRect, getRotation(), &p, flip);
-    }
+    void doDrawShadow(std::shared_ptr<jt::renderTarget> const sptr) const override;
 
-    void doDrawShadow(std::shared_ptr<jt::renderTarget> const sptr) const override
-    {
-        auto const pos
-            = m_position + getShakeOffset() + getOffset() + getCamOffset() + getShadowOffset();
-        SDL_Rect sourceRect { m_sourceRect.left(), m_sourceRect.top(), m_sourceRect.width(),
-            m_sourceRect.height() };
-        float scalex = fabs(m_scale.x());
-        float scaley = fabs(m_scale.y());
-        auto flip = SDL_FLIP_NONE;
-        if (m_scale.x() < 0 && m_scale.y() < 0) {
-            flip = static_cast<SDL_RendererFlip>(SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL);
-        } else if (m_scale.x() < 0 && m_scale.y() >= 0) {
-            flip = SDL_FLIP_HORIZONTAL;
-        } else if (m_scale.x() >= 0 && m_scale.y() < 0) {
-            flip = SDL_FLIP_VERTICAL;
-        }
-        SDL_Rect destRect { static_cast<int>(pos.x()), static_cast<int>(pos.y()),
-            static_cast<int>(m_sourceRect.width() * scalex),
-            static_cast<int>(m_sourceRect.height() * scaley) };
-        SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
-        SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
-        SDL_SetTextureColorMod(
-            m_text.get(), getShadowColor().r(), getShadowColor().g(), getShadowColor().b());
-        SDL_SetTextureAlphaMod(m_text.get(), getShadowColor().a());
-        SDL_RenderCopyEx(sptr.get(), m_text.get(), &sourceRect, &destRect, getRotation(), &p, flip);
-    }
+    void doDrawFlash(std::shared_ptr<jt::renderTarget> const sptr) const override;
+    void doRotate(float /*rot*/) override;
 
-    void doDrawFlash(std::shared_ptr<jt::renderTarget> const sptr) const override
-    {
-        auto const pos = m_position + getShakeOffset() + getOffset() + getCamOffset();
-        SDL_Rect sourceRect { m_sourceRect.left(), m_sourceRect.top(), m_sourceRect.width(),
-            m_sourceRect.height() };
-        float scalex = fabs(m_scale.x());
-        float scaley = fabs(m_scale.y());
-        auto flip = SDL_FLIP_NONE;
-        if (m_scale.x() < 0 && m_scale.y() < 0) {
-            flip = static_cast<SDL_RendererFlip>(SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL);
-        } else if (m_scale.x() < 0 && m_scale.y() >= 0) {
-            flip = SDL_FLIP_HORIZONTAL;
-        } else if (m_scale.x() >= 0 && m_scale.y() < 0) {
-            flip = SDL_FLIP_VERTICAL;
-        }
-        SDL_Rect destRect { static_cast<int>(pos.x()), static_cast<int>(pos.y()),
-            static_cast<int>(m_sourceRect.width() * scalex),
-            static_cast<int>(m_sourceRect.height() * scaley) };
-        SDL_Point p { static_cast<int>(m_origin.x()), static_cast<int>(m_origin.y()) };
-        SDL_SetRenderDrawBlendMode(sptr.get(), SDL_BLENDMODE_BLEND);
-        SDL_SetTextureColorMod(
-            m_textFlash.get(), getFlashColor().r(), getFlashColor().g(), getFlashColor().b());
-        SDL_SetTextureAlphaMod(m_textFlash.get(), getFlashColor().a());
-        SDL_RenderCopyEx(
-            sptr.get(), m_textFlash.get(), &sourceRect, &destRect, getRotation(), &p, flip);
-    }
-
-    void doRotate(float /*rot*/) override { }
-}; // namespace jt
+    SDL_Rect getDestRect(jt::vector2 const& positionOffset = jt::vector2 { 0.0f, 0.0f }) const;
+    SDL_Rect getSourceRect() const;
+    void setSDLColor(jt::color const& col) const;
+};
 
 } // namespace jt
 
