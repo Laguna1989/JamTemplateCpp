b5b789b08539f8c3d25441f17618d6da22624d4b
Simon Weis <laguna.ws@gmail.com>
2020-12-22 11:44:49 +0100

    Add sound implementation

---
+++ src/JamTemplate/common/Sound_base.hpp
@@ -0,0 +1,109 @@
+﻿#ifndef GUARD_JAMTEMPLATE_SOUND_BASE_GUARD_HPP_12345
+#define GUARD_JAMTEMPLATE_SOUND_BASE_GUARD_HPP_12345
+
+#include <string>
+
+namespace jt {
+
+class SoundBase {
+public:
+    void load(std::string const& fileName)
+    {
+        doLoad(fileName);
+        m_isInitialized = true;
+    }
+
+    bool isInitialized() const { return m_isInitialized; }
+
+    void play()
+    {
+        checkInitialized("Can not call play() of uninitialized sound. Call load().");
+        doPlay();
+    }
+    bool isPlaying() const
+    {
+        checkInitialized("Can not call isPlaying() of uninitialized sound. Call load()!");
+        return doIsPlaying();
+    }
+
+    // returns the volume of the sound in the range [0, 100]
+    float getVolume() const
+    {
+        checkInitialized("Can not call getVolume() of uninitialized sound. Call load()!");
+        return doGetVolume();
+    }
+
+    // set the volume of the sound in the range [0, 100]
+    void setVolume(float newVolume)
+    {
+        checkInitialized("Can not call setVolume() of uninitialized sound. Call load()!");
+        if (newVolume < 0.0f || newVolume > 100.0f) {
+            throw std::invalid_argument { "volume " + std::to_string(newVolume)
+                + " is not allowd. Volume has to be in range [0,100]." };
+        }
+        doSetVolume(newVolume);
+    }
+
+    void stop()
+    {
+        checkInitialized("Can not call stop() of uninitialized sound. Call load()!");
+        doStop();
+    }
+
+    void setLoop(bool doLoop)
+    {
+        checkInitialized("Can not call setLoop() of uninitialied sound. Call load()!");
+        doSetLoop(doLoop);
+    }
+
+    bool getLoop()
+    {
+        checkInitialized("Can not call getLoop() of uninitialied sound. Call load()!");
+        return doGetLoop();
+    }
+
+    // returns the length of the sound in seconds
+    float getDuration() const
+    {
+        checkInitialized("Can not call getDuration() of uninitialied sound. Call load()!");
+        return doGetDuration();
+    }
+
+    // returns the current playing position of the sound in seconds
+    float getPosition() const
+    {
+        checkInitialized("Can not call getDuration() of uninitialied sound. Call load()!");
+        return doGetPosition();
+    }
+
+protected:
+    void checkInitialized(std::string const& error) const
+    {
+        if (!isInitialized()) {
+            throw std::logic_error { error.c_str() };
+        }
+    }
+
+private:
+    bool m_isInitialized { false };
+
+    virtual void doLoad(std::string const& fileName) = 0;
+    virtual bool doIsPlaying() const = 0;
+
+    virtual void doPlay() = 0;
+    virtual void doStop() = 0;
+
+    virtual float doGetVolume() const = 0;
+    virtual void doSetVolume(float newVolume) = 0;
+
+    virtual void doSetLoop(bool doLoop) = 0;
+    virtual bool doGetLoop(void) = 0;
+
+    virtual float doGetDuration() const = 0;
+
+    virtual float doGetPosition() const = 0;
+}; // namespace jt
+
+} // namespace jt
+
+#endif
---
+++ src/JamTemplate/sdl/Sound.cpp
@@ -0,0 +1,51 @@
+﻿#include "Sound.hpp"
+#include <exception>
+#include <iostream>
+
+namespace jt {
+
+void Sound::doLoad(std::string const& fileName)
+{
+    wave = std::shared_ptr<Mix_Chunk>(
+        Mix_LoadWAV(fileName.c_str()), [](Mix_Chunk* wave) { Mix_FreeChunk(wave); });
+
+    if (!wave) {
+        throw std::invalid_argument { "cannot load sound from file " + fileName };
+    }
+}
+
+bool Sound::doIsPlaying() const { return m_playing; }
+
+void Sound::doPlay()
+{
+    int loops = m_loop ? -1 : 0;
+    m_channel = Mix_PlayChannel(-1, wave.get(), loops);
+    m_playing = (m_channel != -1);
+}
+void Sound::doStop()
+{
+    if (m_channel != -1) {
+        Mix_Pause(m_channel);
+    }
+    m_playing = false;
+}
+
+float Sound::doGetVolume() const { return m_volume; }
+void Sound::doSetVolume(float newVolume)
+{
+    m_volume = newVolume;
+    int v = static_cast<int>(128 * newVolume / 100.0f);
+    Mix_VolumeChunk(wave.get(), v);
+}
+
+void Sound::doSetLoop(bool doLoop) { m_loop = doLoop; }
+bool Sound::doGetLoop(void) { return m_loop; }
+
+float Sound::doGetDuration() const { return static_cast<double>(wave->alen) / 176.4 / 1000.0; }
+float Sound::doGetPosition() const
+{
+    std::cout << "getPosition() not supported by web audio." << std::endl;
+    return 0;
+}
+
+} // namespace jt
