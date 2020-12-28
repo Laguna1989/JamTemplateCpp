a4e1c2c4dd495053c90f22094cc8c3c0ad22a30f
Simon Weis <laguna.ws@gmail.com>
2020-12-28 21:06:47 +0100

    Fix capitalization issue in filename

--- src/JamTemplate/sdl/Color_lib.hpp
+++ src/JamTemplate/sdl/Color_lib.hpp
@@ -1,7 +1,7 @@
 ﻿#ifndef GUARD_JAMTEMPLATE_COLOR_LIB_GUARD_HPP
 #define GUARD_JAMTEMPLATE_COLOR_LIB_GUARD_HPP
 
-#include "color_base.hpp"
+#include "Color_base.hpp"
 #include <SDL.h>
 
 namespace jt {
