29162eff2438e71790db0c09402e89000add6ef8
Simon Weis <laguna.ws@gmail.com>
2021-04-16 19:46:50 +0200

    Add include for uint32_t

--- src/JamTemplate/common/KeyCodes.cpp
+++ src/JamTemplate/common/KeyCodes.cpp
@@ -1,11 +1,12 @@
-#include "KeyCodes.hpp"
+﻿#include "KeyCodes.hpp"
+#include <cstdint>
 
 namespace jt {
 std::vector<jt::KeyCode> getAllKeys()
 {
-    auto const maxValue = static_cast<uint32_t>(jt::KeyCode::KeyCount);
+    auto const maxValue = static_cast<std::uint32_t>(jt::KeyCode::KeyCount);
     std::vector<jt::KeyCode> values(maxValue, jt::KeyCode::A);
-    for (uint32_t i = 0U; i != maxValue; ++i) {
+    for (std::uint32_t i = 0U; i != maxValue; ++i) {
         values.at(i) = static_cast<jt::KeyCode>(i);
     }
     return values;
@@ -13,9 +14,9 @@ std::vector<jt::KeyCode> getAllKeys()
 
 std::vector<jt::MouseButtonCode> getAllButtons()
 {
-    auto const maxValue = static_cast<uint32_t>(jt::MouseButtonCode::ButtonCount);
+    auto const maxValue = static_cast<std::uint32_t>(jt::MouseButtonCode::ButtonCount);
     std::vector<jt::MouseButtonCode> values(maxValue, jt::MouseButtonCode::MBLeft);
-    for (uint32_t i = 0U; i != maxValue; ++i) {
+    for (std::uint32_t i = 0U; i != maxValue; ++i) {
         values.at(i) = static_cast<jt::MouseButtonCode>(i);
     }
     return values;
