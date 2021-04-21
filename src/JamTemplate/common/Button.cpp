57a547547a9f030ff8746297698f525138352d86
Simon Weis <laguna.ws@gmail.com>
2021-04-21 19:14:19 +0200

    Split button in cpp and hpp

---
+++ src/JamTemplate/common/Button.cpp
@@ -0,0 +1,89 @@
+﻿#include "Button.hpp"
+#include "Animation.hpp"
+#include "DrawableImpl.hpp"
+#include "GameInterface.hpp"
+#include "GameObject.hpp"
+#include "InputManager.hpp"
+#include "Vector.hpp"
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace jt {
+
+Button::Button(jt::Vector2u s)
+{
+    std::string buttonImageName = "#b#" + std::to_string(s.x()) + "#" + std::to_string(s.y());
+    m_background = std::make_shared<jt::Animation>();
+    m_background->add(buttonImageName, "normal", s, { 0 }, 1);
+    m_background->add(buttonImageName, "over", s, { 1 }, 1);
+    m_background->add(buttonImageName, "down", s, { 2 }, 1);
+    m_background->play("normal");
+    m_background->setIgnoreCamMovement(true);
+}
+Button::~Button()
+{
+    m_drawable = nullptr;
+    m_background = nullptr;
+    m_callbacks.clear();
+}
+
+void Button::setDrawable(std::shared_ptr<DrawableImpl> sprt) { m_drawable = sprt; }
+
+void Button::addCallback(std::function<void(void)> cb) { m_callbacks.push_back(cb); }
+
+void Button::clearCallbacks() { m_callbacks.clear(); }
+size_t Button::getCallbackCount() const { return m_callbacks.size(); }
+
+bool Button::IsMouseOver() { return isOver(InputManager::getMousePositionScreen()); }
+
+void Button::setVisible(bool v) { m_visible = v; }
+
+void Button::setPosition(jt::Vector2 const& v) { m_pos = v; }
+
+void Button::doDraw() const
+{
+    m_background->draw(getGame()->getRenderTarget());
+    if (m_drawable)
+        m_drawable->draw(getGame()->getRenderTarget());
+}
+
+bool Button::isOver(jt::Vector2 const& mp)
+{
+    float px = m_background->getPosition().x();
+    float py = m_background->getPosition().y();
+
+    float w = m_background->getGlobalBounds().width();
+    float h = m_background->getGlobalBounds().height();
+    return (mp.x() > px && mp.x() <= px + w && mp.y() > py && mp.y() <= py + h);
+}
+
+void Button::doUpdate(float elapsed)
+{
+    m_background->update(elapsed);
+    m_background->setPosition(m_pos);
+    if (m_drawable) {
+        m_drawable->setPosition(m_pos);
+        m_drawable->update(elapsed);
+    }
+
+    if (isOver(jt::InputManager::getMousePositionScreen())) {
+        if (jt::InputManager::pressed(jt::MouseButtonCode::MBLeft)) {
+            m_background->play("down");
+        } else {
+            m_background->play("over");
+        }
+
+        if (jt::InputManager::justReleased(jt::MouseButtonCode::MBLeft)) {
+            if (m_visible) {
+                for (auto& cb : m_callbacks) {
+                    cb();
+                }
+            }
+        }
+    } else {
+        m_background->play("normal");
+    }
+}
+
+} // namespace jt
--- src/JamTemplate/common/Button.hpp
+++ src/JamTemplate/common/Button.hpp
@@ -1,62 +1,37 @@
 ﻿#ifndef GUARD_JAMTEMPLATE_BUTTON_HPP_GUARD
 #define GUARD_JAMTEMPLATE_BUTTON_HPP_GUARD
 
-#include "Animation.hpp"
-#include "DrawableImpl.hpp"
-#include "GameInterface.hpp"
 #include "GameObject.hpp"
-#include "InputManager.hpp"
 #include "Vector.hpp"
 #include <functional>
 #include <memory>
-#include <string>
 
 namespace jt {
+class Animation;
+class DrawableImpl;
+
 class Button : public GameObject {
 public:
     using Sptr = std::shared_ptr<Button>;
 
-    Button(jt::Vector2u s = jt::Vector2u { 16, 16 })
-    {
-        std::string buttonImageName = "#b#" + std::to_string(s.x()) + "#" + std::to_string(s.y());
-        m_background = std::make_shared<jt::Animation>();
-        m_background->add(buttonImageName, "normal", s, { 0 }, 1);
-        m_background->add(buttonImageName, "over", s, { 1 }, 1);
-        m_background->add(buttonImageName, "down", s, { 2 }, 1);
-        m_background->play("normal");
-        m_background->setIgnoreCamMovement(true);
-    }
-    ~Button()
-    {
-        // std::cout << "button destructor\n";
-        // std::cout << m_drawable << "\n";
-        m_drawable = nullptr;
-        // std::cout << "button destructor half 1\n";
-        m_background = nullptr;
-        // std::cout << "button destructor half 2\n";
-        m_callbacks.clear();
-        // std::cout << "button destructor end\n";
-    }
+    Button(jt::Vector2u s = jt::Vector2u { 16, 16 });
+    ~Button();
 
     Button(const Button& b) = delete;
     Button(Button&& b) = default;
 
-    void setDrawable(DrawableImpl::Sptr sprt) { m_drawable = sprt; }
+    void setDrawable(std::shared_ptr<DrawableImpl> sprt);
 
-    void addCallback(std::function<void(void)> cb) { m_callbacks.push_back(cb); }
+    void addCallback(std::function<void(void)> cb);
 
-    void clearCallbacks() { m_callbacks.clear(); }
-    size_t getCallbackCount() const { return m_callbacks.size(); }
+    void clearCallbacks();
+    size_t getCallbackCount() const;
 
-    bool IsMouseOver()
-    {
-        return isOver(
-            InputManager::getMousePositionScreen().x(), InputManager::getMousePositionScreen().y());
-    }
+    bool IsMouseOver();
 
-    void setVisible(bool v) { m_visible = v; }
+    void setVisible(bool v);
 
-    void setPosition(jt::Vector2 const& v) { m_pos = v; }
+    void setPosition(jt::Vector2 const& v);
 
 private:
     std::shared_ptr<Animation> m_background;
@@ -66,53 +41,11 @@ private:
 
     bool m_visible { true };
 
-    void doDraw() const override
-    {
-        m_background->draw(getGame()->getRenderTarget());
-        if (m_drawable)
-            m_drawable->draw(getGame()->getRenderTarget());
-    }
+    void doDraw() const override;
 
-    bool isOver(float mx, float my)
-    {
-        float px = m_background->getPosition().x();
-        float py = m_background->getPosition().y();
+    bool isOver(jt::Vector2 const& mp);
 
-        float w = m_background->getGlobalBounds().width();
-        float h = m_background->getGlobalBounds().height();
-        return (mx > px && mx <= px + w && my > py && my <= py + h);
-    }
-
-    void doUpdate(float elapsed) override
-    {
-        m_background->update(elapsed);
-        m_background->setPosition(m_pos);
-        if (m_drawable) {
-            m_drawable->setPosition(m_pos);
-            m_drawable->update(elapsed);
-        }
-
-        if (isOver(jt::InputManager::getMousePositionScreen().x(),
-                jt::InputManager::getMousePositionScreen().y())) {
-            if (jt::InputManager::pressed(jt::MouseButtonCode::MBLeft)) {
-                m_background->play("down");
-            } else {
-                m_background->play("over");
-            }
-
-            if (jt::InputManager::justReleased(jt::MouseButtonCode::MBLeft)) {
-                if (m_visible) {
-                    for (auto& cb : m_callbacks) {
-                        // std::cout << "callback\n";
-                        cb();
-                    }
-                }
-            }
-        } else {
-            // std::cout << "not over" << std::endl;
-            m_background->play("normal");
-        }
-    }
+    void doUpdate(float elapsed) override;
 };
 } // namespace jt
 
