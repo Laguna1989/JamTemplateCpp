7ec438c4d40644a3d91598a096f7fcf77ca48772
Simon Weis <laguna.ws@gmail.com>
2021-05-18 19:24:54 +0200

    Add two more test files

---
+++ src/UnitTests/BarTest.cpp
@@ -0,0 +1,26 @@
+﻿#include "Bar.hpp"
+#include <gtest/gtest.h>
+
+TEST(BarTest, BarInitialValues)
+{
+    float const x = 64.0f;
+    float const y = 16.0f;
+    jt::Bar b { x, y };
+    EXPECT_FLOAT_EQ(b.getLocalBounds().width(), x);
+    EXPECT_FLOAT_EQ(b.getLocalBounds().height(), y);
+}
+
+TEST(BarTest, PositiveMaxValueDoesNotTriggerAssertion)
+{
+    jt::Bar b { 5.0f, 10.0f };
+
+    b.setMaxValue(15.0f);
+    SUCCEED();
+}
+
+TEST(BarTest, NegativeMaxValueTriggersAssertion)
+{
+    jt::Bar b { 5.0f, 10.0f };
+
+    EXPECT_DEATH(b.setMaxValue(-5.0f), "");
+}
---
+++ src/UnitTests/DrawableHelpersTest.cpp
@@ -0,0 +1,25 @@
+﻿#include "DrawableHelpers.hpp"
+#include "Shape.hpp"
+#include "Sprite.hpp"
+#include <gtest/gtest.h>
+
+TEST(DrawableHelpersTest, CreateRectShape)
+{
+    float x = 100.0f;
+    float y = 200.0f;
+    auto const shape = jt::dh::createRectShape({ x, y }, jt::colors::Green);
+    ASSERT_NE(shape, nullptr);
+    EXPECT_FLOAT_EQ(shape->getLocalBounds().width(), x);
+    EXPECT_FLOAT_EQ(shape->getLocalBounds().height(), y);
+    EXPECT_EQ(shape->getColor(), jt::colors::Green);
+}
+
+TEST(DrawableHelpersTest, createVignette)
+{
+    float x = 150.0f;
+    float y = 50.0f;
+    auto const vignette = jt::dh::createVignette({ x, y });
+    ASSERT_NE(vignette, nullptr);
+    EXPECT_FLOAT_EQ(vignette->getLocalBounds().width(), x);
+    EXPECT_FLOAT_EQ(vignette->getLocalBounds().height(), y);
+}
