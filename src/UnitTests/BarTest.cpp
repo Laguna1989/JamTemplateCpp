960835d3dd04ae6d99c523d834db59eb9a1d7974
Simon Weis <laguna.ws@gmail.com>
2021-06-06 11:26:53 +0200

    Add Additional tests for Bar class

--- src/UnitTests/BarTest.cpp
+++ src/UnitTests/BarTest.cpp
@@ -41,6 +41,34 @@ TEST(BarTest, SetCurrentValueSetsCorrectValue)
     EXPECT_FLOAT_EQ(b.getValueFraction(), current / max);
 }
 
+TEST(BarTest, SetCurrentValueBelowZero)
+{
+    jt::Bar b { 5.0f, 10.0f };
+
+    float const max = 100.0f;
+    b.setMaxValue(max);
+
+    float const current = -1.5f;
+    b.setCurrentValue(current);
+
+    EXPECT_FLOAT_EQ(b.getCurrentValue(), 0.0f);
+    EXPECT_FLOAT_EQ(b.getValueFraction(), 0.0f);
+}
+
+TEST(BarTest, SetCurrentValueAboveMaxValue)
+{
+    jt::Bar b { 5.0f, 10.0f };
+
+    float const max = 100.0f;
+    b.setMaxValue(max);
+
+    float const current = max + 15.0f;
+    b.setCurrentValue(current);
+
+    EXPECT_FLOAT_EQ(b.getCurrentValue(), max);
+    EXPECT_FLOAT_EQ(b.getValueFraction(), 1.0f);
+}
+
 TEST(BarTest, ColorsCanBeSet)
 {
     jt::Bar b { 5.0f, 10.0f };
@@ -62,4 +90,67 @@ TEST(BarTest, CanBeDrawnWithNullptrRenderTarget)
     b.draw(nullptr);
 
     SUCCEED();
+}
+
+TEST(BarTest, VerticalBar)
+{
+    jt::Bar b { 5.0f, 100.0f, false };
+    b.update(0.1f);
+    b.draw(nullptr);
+
+    SUCCEED();
+}
+
+TEST(BarTest, SetFrontColor)
+{
+    jt::Bar b { 5.0f, 100.0f };
+    b.setFrontColor(jt::colors::Red);
+    ASSERT_EQ(b.getColor(), jt::colors::Red);
+}
+
+TEST(BarTest, SetBackgroundColor)
+{
+    jt::Bar b { 5.0f, 100.0f };
+    b.setBackColor(jt::colors::Yellow);
+    SUCCEED();
+}
+
+TEST(BarTest, RotateHasNoEffect)
+{
+    jt::Bar b { 10.0f, 10.0f };
+    b.setRotation(22.0f);
+    SUCCEED();
+}
+
+TEST(BarTest, SetPositionSetsCorrectPosition)
+{
+    jt::Bar b { 10.0f, 10.0f };
+    b.setPosition(jt::Vector2 { 100.0f, 100.0f });
+    auto const result = b.getPosition();
+    EXPECT_FLOAT_EQ(result.x(), 100.0f);
+    EXPECT_FLOAT_EQ(result.y(), 100.0f);
+}
+
+TEST(BarTest, FlashRaisesException)
+{
+    jt::Bar b { 10.0f, 10.0f };
+    EXPECT_THROW(b.flash(1.0f), std::logic_error);
+}
+
+TEST(BarTest, ScaleBarSetsScale)
+{
+    jt::Bar b { 10.0f, 10.0f };
+    b.setScale(jt::Vector2 { 100.0f, 100.0f });
+    auto const result = b.getScale();
+    EXPECT_FLOAT_EQ(result.x(), 100.0f);
+    EXPECT_FLOAT_EQ(result.y(), 100.0f);
+}
+
+TEST(BarTest, SetOriginSetsOrigin)
+{
+    jt::Bar b { 10.0f, 10.0f };
+    b.setOrigin(jt::Vector2 { 100.0f, 100.0f });
+    auto const result = b.getOrigin();
+    EXPECT_FLOAT_EQ(result.x(), 100.0f);
+    EXPECT_FLOAT_EQ(result.y(), 100.0f);
 }
