a9f10e6e4caabdde23bef8f4c786768f82d2d212
Simon Weis <laguna.ws@gmail.com>
2021-09-06 16:40:31 +0200

    Exclude system input files from coverage

--- src/jamtemplate/sfml/keyboard_input_lib.cpp
+++ src/jamtemplate/sfml/keyboard_input_lib.cpp
@@ -3,6 +3,8 @@
 
 namespace jt {
 
+// it is not possible to get keyboard input for automated unittests
+// GCOV_EXCL_START
 namespace {
 sf::Keyboard::Key toLib(jt::KeyCode key) { return static_cast<sf::Keyboard::Key>(key); }
 } // namespace
@@ -12,4 +14,6 @@ bool libKeyValue(jt::KeyCode b)
     auto const libkey = toLib(b);
     return sf::Keyboard::isKeyPressed(libkey);
 }
+
+// GCOV_EXCL_STOP
 } // namespace jt
