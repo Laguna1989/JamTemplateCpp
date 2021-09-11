567a2108238aaf21794b925335109a014260c381
Simon Weis <laguna.ws@gmail.com>
2021-09-11 08:19:44 +0200

    Rework text tests

--- src/unittests/text_test.cpp
+++ src/unittests/text_test.cpp
@@ -14,8 +14,8 @@ jt::Text getText()
 TEST(TextTest, InitialValues)
 {
     jt::Text t = getText();
-    EXPECT_FLOAT_EQ(t.getLocalBounds().width(), 0.0f);
-    EXPECT_FLOAT_EQ(t.getLocalBounds().height(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getLocalBounds().width(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getLocalBounds().height(), 0.0f);
 }
 TEST(TextTest, Destructor)
 {
@@ -34,7 +34,7 @@ TEST(TextTest, SetText)
 {
     jt::Text t = getText();
     t.setText("test1234");
-    EXPECT_EQ(t.getText(), "test1234");
+    ASSERT_EQ(t.getText(), "test1234");
 }
 
 TEST(TextTest, LoadInvalidFont)
@@ -48,8 +48,8 @@ TEST(TextTest, LoadInvalidFont)
 TEST(TextTest, InitialPosition)
 {
     jt::Text t = getText();
-    EXPECT_FLOAT_EQ(t.getPosition().x(), 0.0f);
-    EXPECT_FLOAT_EQ(t.getPosition().y(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getPosition().x(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getPosition().y(), 0.0f);
 }
 
 TEST(TextTest, PositionAfterSetPosition)
@@ -57,13 +57,13 @@ TEST(TextTest, PositionAfterSetPosition)
     jt::Text t = getText();
     jt::Vector2 newPos { 200.0f, 150.0f };
     t.setPosition(newPos);
-    EXPECT_EQ(t.getPosition(), newPos);
+    ASSERT_EQ(t.getPosition(), newPos);
 }
 
 TEST(TextTest, InitialColor)
 {
     jt::Text t = getText();
-    EXPECT_EQ(t.getColor(), jt::colors::White);
+    ASSERT_EQ(t.getColor(), jt::colors::White);
 }
 
 TEST(TextTest, ColorAfterSetColor)
@@ -71,13 +71,13 @@ TEST(TextTest, ColorAfterSetColor)
     jt::Text t = getText();
     jt::Color newCol { jt::colors::Blue };
     t.setColor(newCol);
-    EXPECT_EQ(t.getColor(), newCol);
+    ASSERT_EQ(t.getColor(), newCol);
 }
 
 TEST(TextTest, InitialFlashColor)
 {
     jt::Text t = getText();
-    EXPECT_EQ(t.getFlashColor(), jt::colors::White);
+    ASSERT_EQ(t.getFlashColor(), jt::colors::White);
 }
 
 TEST(TextTest, FlashColorAfterSetFlashColor)
@@ -85,21 +85,21 @@ TEST(TextTest, FlashColorAfterSetFlashColor)
     jt::Text t = getText();
     jt::Color newCol { jt::colors::Blue };
     t.setFlashColor(newCol);
-    EXPECT_EQ(t.getFlashColor(), newCol);
+    ASSERT_EQ(t.getFlashColor(), newCol);
 }
 
 TEST(TextTest, InitialLocalBounds)
 {
     jt::Text t = getText();
     jt::Rect expectedBounds { 0.0f, 0.0f, 0.0f, 0.0f };
-    EXPECT_EQ(t.getLocalBounds(), expectedBounds);
+    ASSERT_EQ(t.getLocalBounds(), expectedBounds);
 }
 
 TEST(TextTest, InitialGlobalBounds)
 {
     jt::Text t = getText();
     jt::Rect expectedBounds { 0.0f, 0.0f, 0.0f, 0.0f };
-    EXPECT_EQ(t.getGlobalBounds(), expectedBounds);
+    ASSERT_EQ(t.getGlobalBounds(), expectedBounds);
 }
 
 TEST(TextTest, LocalBoundsWithLoadedSprite)
@@ -107,14 +107,14 @@ TEST(TextTest, LocalBoundsWithLoadedSprite)
     jt::Text t = getText();
     t.setText("test1234");
     jt::Rect expectedBounds { 0.0f, 0.0f, 0.0f, 0.0f };
-    EXPECT_NE(t.getLocalBounds(), expectedBounds);
+    ASSERT_NE(t.getLocalBounds(), expectedBounds);
 }
 
 TEST(TextTest, InitialScale)
 {
     jt::Text t = getText();
-    EXPECT_FLOAT_EQ(t.getScale().x(), 1.0f);
-    EXPECT_FLOAT_EQ(t.getScale().y(), 1.0f);
+    ASSERT_FLOAT_EQ(t.getScale().x(), 1.0f);
+    ASSERT_FLOAT_EQ(t.getScale().y(), 1.0f);
 }
 
 TEST(TextTest, ScaleAfterSetScale)
@@ -122,14 +122,14 @@ TEST(TextTest, ScaleAfterSetScale)
     jt::Text t = getText();
     jt::Vector2 newScale { 200.0f, 150.0f };
     t.setScale(newScale);
-    EXPECT_EQ(t.getScale(), newScale);
+    ASSERT_EQ(t.getScale(), newScale);
 }
 
 TEST(TextTest, InitialOrigin)
 {
     jt::Text t = getText();
-    EXPECT_FLOAT_EQ(t.getOrigin().x(), 0.0f);
-    EXPECT_FLOAT_EQ(t.getOrigin().y(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getOrigin().x(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getOrigin().y(), 0.0f);
 }
 
 TEST(TextTest, OriginAfterSetOrigin)
@@ -137,28 +137,32 @@ TEST(TextTest, OriginAfterSetOrigin)
     jt::Text t = getText();
     jt::Vector2 newOrigin { 20.0f, -15.0f };
     t.setOrigin(newOrigin);
-    EXPECT_EQ(t.getOrigin(), newOrigin);
+    ASSERT_EQ(t.getOrigin(), newOrigin);
 }
 
 TEST(TextTest, Update)
 {
     jt::Text t = getText();
-    t.update(0.1f);
-    SUCCEED();
+    ASSERT_NO_THROW(t.update(0.1f));
 }
 
 TEST(TextTest, DrawNullptr)
 {
     jt::Text t = getText();
     t.update(0.0f);
-    t.draw(nullptr);
-    SUCCEED();
+    ASSERT_NO_THROW(t.draw(nullptr));
+}
+
+TEST(TextTest, SetOutline)
+{
+    jt::Text t = getText();
+    t.setOutline(2.0f, jt::colors::Red);
 }
 
 TEST(TextTest, InitialRotation)
 {
     jt::Text t = getText();
-    EXPECT_FLOAT_EQ(t.getRotation(), 0.0f);
+    ASSERT_FLOAT_EQ(t.getRotation(), 0.0f);
 }
 
 TEST(TextTest, RotationAfterSetRotation)
@@ -166,5 +170,27 @@ TEST(TextTest, RotationAfterSetRotation)
     jt::Text t = getText();
     float const expectedRotation { 45.0f };
     t.setRotation(expectedRotation);
-    EXPECT_FLOAT_EQ(t.getRotation(), expectedRotation);
+    ASSERT_FLOAT_EQ(t.getRotation(), expectedRotation);
 }
+
+TEST(TextTest, InitialTextAlignDefault)
+{
+    jt::Text t = getText();
+    ASSERT_EQ(t.getTextAlign(), jt::Text::TextAlign::CENTER);
+}
+
+TEST(TextTest, TextAlignAfterSetRight)
+{
+    jt::Text t = getText();
+    t.SetTextAlign(jt::Text::TextAlign::RIGHT);
+    ASSERT_EQ(t.getTextAlign(), jt::Text::TextAlign::RIGHT);
+}
+
+TEST(TextTest, TextAlignAfterSetLeft)
+{
+    jt::Text t = getText();
+    t.SetTextAlign(jt::Text::TextAlign::LEFT);
+    ASSERT_EQ(t.getTextAlign(), jt::Text::TextAlign::LEFT);
+}
+
+
