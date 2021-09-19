d449fb70ea6ff5c1416cd84f4ff30ad0f648d684
Simon Weis <laguna.ws@gmail.com>
2021-09-19 09:54:56 +0200

    Make ObjectGroup not derive from std::vector

--- src/jamtemplate/common/object_group.hpp
+++ src/jamtemplate/common/object_group.hpp
@@ -15,21 +15,29 @@ namespace jt {
 /// naming convention will differ here from the rest of the project as this is about to mimic the
 /// std::vector interface
 template <typename T>
-class ObjectGroup : public GameObject, public std::vector<std::weak_ptr<T>> {
+class ObjectGroup : public GameObject {
 public:
     using Sptr = std::shared_ptr<ObjectGroup<T>>;
 
     ObjectGroup() = default;
+    auto begin() { return m_data.begin(); }
+    auto end() { return m_data.end(); }
+    auto cbegin() const { return m_data.cbegin(); }
+    auto cend() const { return m_data.cend(); }
+
+    auto size() const { return m_data.size(); }
+    auto at(std::size_t idx) { return m_data.at(idx); }
+    void push_back(std::weak_ptr<T> wptr) { m_data.push_back(wptr); }
+    void emplace_back(std::weak_ptr<T>&& wptr) { m_data.emplace_back(std::move(wptr)); }
 
 private:
     void doUpdate(const float /*elapsed*/) override
     {
-        std::vector<std::weak_ptr<T>>::erase(
-            std::remove_if(std::vector<std::weak_ptr<T>>::begin(),
-                std::vector<std::weak_ptr<T>>::end(),
-                [](std::weak_ptr<T> wptr) { return wptr.expired(); }),
-            std::vector<std::weak_ptr<T>>::end());
+        m_data.erase(std::remove_if(m_data.begin(), m_data.end(),
+                         [](std::weak_ptr<T> wptr) { return wptr.expired(); }),
+            m_data.end());
     }
+    std::vector<std::weak_ptr<T>> m_data;
 };
 
 template <typename T>
