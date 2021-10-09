fc80f14e2336ba8c3d990a0d8bee4ed0e6057abc
Simon Weis <laguna.ws@gmail.com>
2021-10-09 07:19:55 +0200

    Add test for color default constructor

---
+++ src/unittests/color_test.cpp
@@ -0,0 +1,14 @@
+#include "color.hpp"
+#include <gtest/gtest.h>
+#include <type_traits>
+
+TEST(ColorTest, IsDefaultConstructible)
+{
+    ASSERT_TRUE(std::is_default_constructible<jt::Color>::value);
+}
+
+TEST(ColorTest, InitialValues)
+{
+    jt::Color c;
+    ASSERT_EQ(c, jt::colors::Black);
+}
