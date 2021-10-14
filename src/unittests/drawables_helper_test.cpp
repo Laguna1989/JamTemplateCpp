5c4aa25d6d016fdbfde9fdad69c7954d763ba4a1
Simon Weis <laguna.ws@gmail.com>
2021-10-14 15:34:53 +0200

    Clean up drawable helpers test

--- src/unittests/drawables_helper_test.cpp
+++ src/unittests/drawables_helper_test.cpp
@@ -4,12 +4,13 @@
 #include "text.hpp"
 #include <gtest/gtest.h>
 
-TEST(DHRectCreateRectShapeTest, NoNullptr)
+TEST(CreateRectShapeTest, NoNullptr)
 {
     auto const shape = jt::dh::createRectShape({ 1.0f, 2.0f }, jt::colors::Green);
     ASSERT_NE(shape, nullptr);
 }
-TEST(DHRectCreateRectShapeTest, Size)
+
+TEST(RectCreateRectShapeTest, Size)
 {
     jt::Rect expected { 0.0f, 0.0f, 100.0f, 200.0f };
     auto const shape
@@ -18,27 +19,48 @@ TEST(DHRectCreateRectShapeTest, Size)
     ASSERT_EQ(shape->getLocalBounds(), expected);
 }
 
-TEST(DHRectCreateRectShapeTest, Color)
+TEST(CreateRectShapeTest, Color)
 {
     auto const col = jt::colors::Green;
     auto const shape = jt::dh::createRectShape({ 3.0f, 4.0f }, col);
     ASSERT_EQ(shape->getColor(), col);
 }
 
-TEST(DrawableHelpersTest, createVignette)
+TEST(CreateVignetteTest, NoNullptr)
 {
-    float x = 150.0f;
-    float y = 50.0f;
+    float const x = 150.0f;
+    float const y = 50.0f;
     auto const vignette = jt::dh::createVignette({ x, y });
     ASSERT_NE(vignette, nullptr);
+}
+
+TEST(CreateVignetteTest, Size)
+{
+    float const x = 150.0f;
+    float const y = 50.0f;
+    auto const vignette = jt::dh::createVignette({ x, y });
     ASSERT_FLOAT_EQ(vignette->getLocalBounds().width(), x);
     ASSERT_FLOAT_EQ(vignette->getLocalBounds().height(), y);
 }
 
-TEST(DrawableHelpersTest, createText)
+TEST(CreateVignetteTest, Color)
+{
+    float const x = 150.0f;
+    float const y = 50.0f;
+    auto const vignette = jt::dh::createVignette({ x, y });
+    ASSERT_EQ(vignette->getColor(), jt::colors::White);
+}
+
+TEST(CreateTextTest, NoNullptr)
 {
     std::shared_ptr<jt::renderTarget> rt { nullptr };
     auto const text = jt::dh::createText(rt, "assets/font.ttf", 24, jt::colors::Green);
     ASSERT_NE(text, nullptr);
+}
+
+TEST(CreateTextTest, Color)
+{
+    std::shared_ptr<jt::renderTarget> rt { nullptr };
+    auto const text = jt::dh::createText(rt, "assets/font.ttf", 24, jt::colors::Green);
     ASSERT_EQ(text->getColor(), jt::colors::Green);
 }
