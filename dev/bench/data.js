8f9262ba1fd653eb257ec9b243cbbc7e92cbdefe
github-action-benchmark <github@users.noreply.github.com>
2021-10-18 15:02:36 +0000

    add Benchmark (googlecpp) benchmark result for 01885dab61b5160afcf83e7a877c62cb34769dea

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1634569356801,
+  "repoUrl": "https://github.com/Laguna1989/JamTemplateCpp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "laguna.ws@gmail.com",
+            "name": "Simon Weis",
+            "username": "Laguna1989"
+          },
+          "committer": {
+            "email": "laguna.ws@gmail.com",
+            "name": "Simon Weis",
+            "username": "Laguna1989"
+          },
+          "distinct": true,
+          "id": "01885dab61b5160afcf83e7a877c62cb34769dea",
+          "message": "Do not use ccache but gh pages approach",
+          "timestamp": "2021-10-18T17:01:10+02:00",
+          "tree_id": "8ad1109fb68953d1bb568afbc8eb433826106fa0",
+          "url": "https://github.com/Laguna1989/JamTemplateCpp/commit/01885dab61b5160afcf83e7a877c62cb34769dea"
+        },
+        "date": 1634569356329,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_GamestateWithTweeningShapes",
+            "value": 801046.8356510771,
+            "unit": "ns/iter",
+            "extra": "iterations: 791\ncpu: 788417.0012642227 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
