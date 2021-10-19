6ca1c027101e20a5415dd41cdaa9ff23e8c3b112
Simon Weis <laguna.ws@gmail.com>
2021-10-19 16:46:07 +0200

    Add two new test cases for performance test

---
+++ test/integration/performance/startgame.cpp
@@ -0,0 +1,36 @@
+#include "game.hpp"
+#include "game_state.hpp"
+#include "random.hpp"
+#include "shape.hpp"
+#include "tween_color.hpp"
+#include <benchmark/benchmark.h>
+#include <memory>
+#include <render_window.hpp>
+#include <vector>
+
+class StateEmpty : public jt::GameState {
+private:
+    std::shared_ptr<jt::Shape> m_shape;
+    void doInternalCreate() override
+    {
+        m_shape = std::make_shared<jt::Shape>();
+        m_shape->makeRect({ 20.0f, 20.0f });
+    }
+    void doInternalUpdate(float elapsed) override { m_shape->update(0.01f); }
+    void doInternalDraw() const override { m_shape->draw(getGame()->getRenderTarget()); }
+};
+
+static void BM_StartGame(benchmark::State& state)
+{
+
+    for (auto _ : state) {
+        auto game = std::make_shared<jt::Game>(
+            std::make_shared<jt::RenderWindow>(800, 600, "jt_performance"), 1.0f, nullptr, nullptr);
+        auto gs = std::make_shared<StateEmpty>();
+        game->switchState(gs);
+        game->update(0.02f);
+        game->draw();
+    }
+}
+
+BENCHMARK(BM_StartGame);
