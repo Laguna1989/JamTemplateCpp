b33ce0b32c84344dadde502557f18fe39c8f7050
Simon Weis <laguna.ws@gmail.com>
2021-10-28 20:01:03 +0200

    Separate game into gamelib and game, adding gamelib test

---
+++ test/unit/gamelib_test/score_display_test.cpp
@@ -0,0 +1,27 @@
+#include "drawable_helpers.hpp"
+#include "hud/score_display.hpp"
+#include "render_target.hpp"
+#include <gtest/gtest.h>
+#include <memory>
+
+auto create_string(std::string const& string)
+{
+    std::shared_ptr<jt::renderTarget> rt = nullptr;
+    return jt::dh::createText(rt, string, 10, jt::colors::White, "assets/font.ttf");
+}
+
+TEST(ScoreDisplayTest, TextIsNotChangedByAddingItToDisplay)
+{
+    auto const expected_string = "ABCD";
+    auto text = create_string(expected_string);
+    ScoreDisplay sd { text, "" };
+    ASSERT_EQ(text->getText(), expected_string);
+}
+
+TEST(ScoreDisplayTest, TextIsUpdatedOnNotify)
+{
+    auto text = create_string("");
+    ScoreDisplay sd { text, "" };
+    sd.notify(20);
+    ASSERT_EQ(text->getText(), "20");
+}
