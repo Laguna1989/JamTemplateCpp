9394bc7870d919a7087fa3dcb1b35d4d10aec82d
Simon Weis <laguna.ws@gmail.com>
2021-12-03 16:46:24 +0100

    More unit tests for palettes

--- test/unit/jt_test/palette_test.cpp
+++ test/unit/jt_test/palette_test.cpp
@@ -3,25 +3,25 @@
 
 TEST(PaletteTest, InitialSizeIsZero)
 {
-    jt::Palette pal;
+    jt::Palette const pal;
     ASSERT_EQ(pal.size(), 0U);
 }
 
 TEST(PaletteTest, InitialEmpty)
 {
-    jt::Palette pal;
+    jt::Palette const pal;
     ASSERT_TRUE(pal.empty());
 }
 
 TEST(PaletteTest, ConstructorInitialSize)
 {
-    jt::Palette pal { { jt::colors::Black, jt::colors::White } };
+    jt::Palette const pal { { jt::colors::Black, jt::colors::White } };
     ASSERT_EQ(pal.size(), 2U);
 }
 
 TEST(PaletteTest, ConstructorInitialEmpty)
 {
-    jt::Palette pal { { jt::colors::Black, jt::colors::White } };
+    jt::Palette const pal { { jt::colors::Black, jt::colors::White } };
     ASSERT_FALSE(pal.empty());
 }
 
@@ -38,3 +38,90 @@ TEST(PaletteTest, NotEmptyAfterAdd)
     pal = pal.addColor(jt::colors::Black);
     ASSERT_FALSE(pal.empty());
 }
+
+TEST(PaletteTest, FromHEmpty)
+{
+    auto const pal = jt::Palette::createGradientH(0.0f, 0.0f, 0.0f, 0.0f, 0U);
+    ASSERT_TRUE(pal.empty());
+}
+
+TEST(PaletteTest, FromHOneEntryBlack)
+{
+    auto const pal = jt::Palette::createGradientH(0.0f, 0.0f, 0.0f, 0.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Black);
+}
+
+TEST(PaletteTest, FromHOneEntryRed)
+{
+    auto const pal = jt::Palette::createGradientH(0.0f, 0.0f, 100.0f, 100.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Red);
+}
+
+TEST(PaletteTest, FromSOneEntryBlack)
+{
+    auto const pal = jt::Palette::createGradientS(0.0f, 0.0f, 0.0f, 0.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Black);
+}
+
+TEST(PaletteTest, FromSOneEntryRed)
+{
+    auto const pal = jt::Palette::createGradientS(0.0f, 100.0f, 100.0f, 100.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Red);
+}
+
+TEST(PaletteTest, FromVOneEntryBlack)
+{
+    auto const pal = jt::Palette::createGradientV(0.0f, 0.0f, 0.0f, 0.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Black);
+}
+
+TEST(PaletteTest, FromVOneEntryRed)
+{
+    auto const pal = jt::Palette::createGradientV(0.0f, 100.0f, 100.0f, 100.0f, 1U);
+    ASSERT_EQ(pal.size(), 1U);
+
+    auto col = pal.getColor(0);
+    ASSERT_EQ(col, jt::colors::Red);
+}
+
+class PaletteSizeTestFixture : public ::testing::TestWithParam<std::size_t> {
+};
+
+TEST_P(PaletteSizeTestFixture, FromHSize)
+{
+    auto const expectedSize = GetParam();
+    auto const pal = jt::Palette::createGradientH(0.0f, 0.0f, 0.0f, 0.0f, expectedSize);
+    ASSERT_EQ(pal.size(), expectedSize);
+}
+
+TEST_P(PaletteSizeTestFixture, FromSSize)
+{
+    auto const expectedSize = GetParam();
+    auto const pal = jt::Palette::createGradientS(0.0f, 0.0f, 0.0f, 0.0f, expectedSize);
+    ASSERT_EQ(pal.size(), expectedSize);
+}
+
+TEST_P(PaletteSizeTestFixture, FromVSize)
+{
+    auto const expectedSize = GetParam();
+    auto const pal = jt::Palette::createGradientV(0.0f, 0.0f, 0.0f, 0.0f, expectedSize);
+    ASSERT_EQ(pal.size(), expectedSize);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    PaletteSizeTest, PaletteSizeTestFixture, ::testing::Values(0U, 1U, 5U, 100U));
