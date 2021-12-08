8bca0e7603e4cdfdcc941e0771f8f568bca3215b
Simon Weis <laguna.ws@gmail.com>
2021-12-08 16:43:14 +0100

    Fix linux builds

--- test/unit/jt_test/logger_test.cpp
+++ test/unit/jt_test/logger_test.cpp
@@ -4,8 +4,6 @@
 
 template <typename T>
 class LoggerTypedTestFixture : public ::testing::Test {
-public:
-    T logger;
 };
 
 using MyTypes = testing::Types<jt::Logger, jt::null_objects::LoggerNull>;
@@ -13,61 +11,66 @@ TYPED_TEST_SUITE(LoggerTypedTestFixture, MyTypes);
 
 TYPED_TEST(LoggerTypedTestFixture, GetCompleteLogIsEmptyByDefault)
 {
+    TypeParam logger;
     ASSERT_TRUE(logger.getHistory().empty());
 }
 
 TYPED_TEST(LoggerTypedTestFixture, AddNullptrLogTarget)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.addLogTarget(nullptr));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, ActionDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.action(""));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, FatalDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.fatal(""));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, ErrorDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.error(""));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, WarnDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.warning(""));
 }
 
-TYPED_TEST(LoggerTypedTestFixture, InfoDoesNotRaiseException) { ASSERT_NO_THROW(logger.info("")); }
+TYPED_TEST(LoggerTypedTestFixture, InfoDoesNotRaiseException)
+{
+    TypeParam logger;
+    ASSERT_NO_THROW(logger.info(""));
+}
 
 TYPED_TEST(LoggerTypedTestFixture, DebugDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.debug(""));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, VerboseDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.verbose(""));
 }
 
 TYPED_TEST(LoggerTypedTestFixture, SetLogLevelDoesNotRaiseException)
 {
+    TypeParam logger;
     ASSERT_NO_THROW(logger.setLogLevel(LogLevel::LogLevelInfo));
 }
 
-TYPED_TEST(LoggerTypedTestFixture, ClearDoesNotRaiseException) { ASSERT_NO_THROW(logger.clear()); }
-
-/*
-TEST(LogTest, GetCompleteLogAfterLog)
+TYPED_TEST(LoggerTypedTestFixture, ClearDoesNotRaiseException)
 {
-    Logger l;
-    l.debug("bla");
-
-    // TODO Add comparison operator for LogEntry
-    ASSERT_EQ(l.getHistory().at(0).message, "bla");
-    ASSERT_EQ(l.getHistory().at(0).level, LogLevel::LogLevelDebug);
+    TypeParam logger;
+    ASSERT_NO_THROW(logger.clear());
 }
-*/
