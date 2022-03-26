f0a39d8a350550119c519ffab2060e775700dba2
Simon Weis <laguna.ws@gmail.com>
2022-03-26 08:14:29 +0100

    refactor gamepad test code

--- test/integration/demo/gamepad/gamepad_visualizer.cpp
+++ test/integration/demo/gamepad/gamepad_visualizer.cpp
@@ -1,5 +1,64 @@
-//
-// Created by Laguna_HP on 26.03.2022.
-//
-
 #include "gamepad_visualizer.hpp"
+#include "game_interface.hpp"
+
+GamepadVisualizer::GamepadVisualizer(std::size_t id)
+    : m_id { id }
+{
+}
+void GamepadVisualizer::doCreate()
+{
+    backgroundLeft = std::make_shared<jt::Shape>();
+    backgroundLeft->makeRect(jt::Vector2f { 50.0f, 50.0f }, getGame()->gfx().textureManager());
+    backgroundLeft->setColor(jt::colors::Gray);
+    backgroundLeft->setPosition(jt::Vector2f { 25.0f, 25.0f + (10.0f + 50.0f) * m_id });
+
+    backgroundRight = std::make_shared<jt::Shape>();
+    backgroundRight->makeRect(jt::Vector2f { 50.0f, 50.0f }, getGame()->gfx().textureManager());
+    backgroundRight->setColor(jt::colors::Gray);
+    backgroundRight->setPosition(
+        jt::Vector2f { 25.0f + 50.0f + 10.0f, 25.0f + (10.0f + 50.0f) * m_id });
+
+    dotLeft = std::make_shared<jt::Shape>();
+    dotLeft->makeRect(jt::Vector2f { 3.0f, 3.0f }, getGame()->gfx().textureManager());
+    dotLeft->setOffset(jt::Vector2f { -1.5f, -1.5f });
+    dotLeft->setColor(jt::colors::Red);
+
+    dotRight = std::make_shared<jt::Shape>();
+    dotRight->makeRect(jt::Vector2f { 3.0f, 3.0f }, getGame()->gfx().textureManager());
+    dotRight->setOffset(jt::Vector2f { -1.5f, -1.5f });
+    dotRight->setColor(jt::colors::Red);
+}
+
+void GamepadVisualizer::doUpdate(float const elapsed)
+{
+    backgroundLeft->update(elapsed);
+    backgroundRight->update(elapsed);
+    jt::Vector2f const halfsize { backgroundLeft->getLocalBounds().width * 0.5f,
+        backgroundLeft->getLocalBounds().height * 0.5f };
+
+    auto const gpl = getGame()->input().gamepad(m_id)->getAxis(jt::GamepadAxisCode::ALeft);
+    dotLeft->setPosition(backgroundLeft->getPosition() + halfsize
+        + jt::Vector2f { gpl.x * halfsize.x, gpl.y * halfsize.y });
+    dotLeft->update(elapsed);
+
+    auto const gpr = getGame()->input().gamepad(m_id)->getAxis(jt::GamepadAxisCode::ARight);
+    dotRight->setPosition(backgroundRight->getPosition() + halfsize
+        + jt::Vector2f { gpr.x * halfsize.x, gpr.y * halfsize.y });
+    dotRight->update(elapsed);
+
+    if (getGame()->input().gamepad(m_id)->justPressed(jt::GamepadButtonCode::GPA)) {
+        backgroundLeft->flash(0.25f);
+    }
+    if (getGame()->input().gamepad(m_id)->justPressed(jt::GamepadButtonCode::GPB)) {
+        backgroundRight->flash(0.125f);
+    }
+}
+
+void GamepadVisualizer::doDraw() const
+{
+    backgroundLeft->draw(getGame()->gfx().target());
+    backgroundRight->draw(getGame()->gfx().target());
+
+    dotLeft->draw(getGame()->gfx().target());
+    dotRight->draw(getGame()->gfx().target());
+}
--- test/integration/demo/gamepad/state_gamepad.cpp
+++ test/integration/demo/gamepad/state_gamepad.cpp
@@ -1,54 +1,15 @@
 #include "state_gamepad.hpp"
 #include "game_interface.hpp"
-#include "shape.hpp"
+#include "gamepad_visualizer.hpp"
 
 void StateGamepad::doInternalCreate()
 {
-    m_backgroundShape = std::make_shared<jt::Shape>();
-    m_backgroundShape->makeRect(jt::Vector2f { 50.0f, 50.0f }, getGame()->gfx().textureManager());
-    m_backgroundShape->setColor(jt::colors::Gray);
-
-    m_dotShape = std::make_shared<jt::Shape>();
-    m_dotShape->makeRect(jt::Vector2f { 3.0f, 3.0f }, getGame()->gfx().textureManager());
-    m_dotShape->setOffset(jt::Vector2f { -1.5f, -1.5f });
-    m_dotShape->setColor(jt::colors::Red);
-}
-
-void StateGamepad::doInternalUpdate(float elapsed)
-{
-    if (getGame()->input().gamepad(0)->justPressed(jt::GamepadButtonCode::GPA)) {
-        m_backgroundShape->flash(0.25f);
-    }
-    m_backgroundShape->update(elapsed);
-    m_dotShape->update(elapsed);
-}
-
-void StateGamepad::doInternalDraw() const
-{
     for (auto i = 0; i != 3; ++i) {
-        m_backgroundShape->setPosition(jt::Vector2f { 25.0f, 25.0f + (10.0f + 50.0f) * i });
-        m_backgroundShape->update(0.0f);
-        m_backgroundShape->draw(getGame()->gfx().target());
-
-        auto const gp0l = getGame()->input().gamepad(i)->getAxis(jt::GamepadAxisCode::ALeft);
-        jt::Vector2f const halfsize { m_backgroundShape->getLocalBounds().width * 0.5f,
-            m_backgroundShape->getLocalBounds().height * 0.5f };
-        m_dotShape->setPosition(m_backgroundShape->getPosition() + halfsize
-            + jt::Vector2f { gp0l.x * halfsize.x, gp0l.y * halfsize.y });
-        m_dotShape->update(0.0f);
-
-        m_dotShape->draw(getGame()->gfx().target());
-
-        m_backgroundShape->setPosition(
-            jt::Vector2f { 25.0f + 10.0f + 50.0f, 25.0f + (10.0f + 50.0f) * i });
-        m_backgroundShape->update(0.0f);
-        m_backgroundShape->draw(getGame()->gfx().target());
-
-        auto const gp0r = getGame()->input().gamepad(i)->getAxis(jt::GamepadAxisCode::ARight);
-        m_dotShape->setPosition(m_backgroundShape->getPosition() + halfsize
-            + jt::Vector2f { gp0r.x * halfsize.x, gp0r.y * halfsize.y });
-        m_dotShape->update(0.0f);
-
-        m_dotShape->draw(getGame()->gfx().target());
+        auto viz = std::make_shared<GamepadVisualizer>(i);
+        add(viz);
     }
 }
+
+void StateGamepad::doInternalUpdate(float elapsed) { }
+
+void StateGamepad::doInternalDraw() const { }
