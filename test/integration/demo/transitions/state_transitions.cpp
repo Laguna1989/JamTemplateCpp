95ee16d447a8360b9fdfa64b80026ba10261d4bb
Simon Weis <laguna.ws@gmail.com>
2022-04-07 20:12:53 +0200

    Add demo for state transitions

---
+++ test/integration/demo/transitions/state_transitions.cpp
@@ -0,0 +1,34 @@
+#include "state_transitions.hpp"
+#include "../state_select.hpp"
+#include "game_interface.hpp"
+#include "imgui.h"
+#include "state_manager/state_manager_transition_fade_to_black.hpp"
+#include "state_manager/state_manager_transition_horizontal_bars.hpp"
+#include "state_manager/state_manager_transition_none.hpp"
+
+void StateTransitions::doInternalCreate() { }
+void StateTransitions::doInternalUpdate(float elapsed) { }
+void StateTransitions::doInternalDraw() const
+{
+    ImGui::Begin("Transitions");
+    if (ImGui::Button("None")) {
+        getGame()->stateManager().setTransition(std::make_shared<jt::StateManagerTransitionNone>());
+        getGame()->stateManager().switchState(std::make_shared<StateSelect>());
+    }
+    if (ImGui::Button("Fade2Black")) {
+        getGame()->stateManager().setTransition(
+            std::make_shared<jt::StateManagerTransitionFadeToBlack>(
+                getGame()->gfx().window().getSize(), getGame()->gfx().textureManager(), 0.5f));
+        getGame()->stateManager().switchState(std::make_shared<StateSelect>());
+    }
+    if (ImGui::Button("Horizontal Bars")) {
+        getGame()->stateManager().setTransition(
+            std::make_shared<jt::StateManagerTransitionHorizontalBars>(
+                jt::Vector2f {
+                    getGame()->gfx().window().getSize().x / getGame()->gfx().camera().getZoom(),
+                    32 },
+                10, 1.0f, getGame()->gfx().textureManager()));
+        getGame()->stateManager().switchState(std::make_shared<StateSelect>());
+    }
+    ImGui::End();
+}
