ccf618617ea58a423b813cdfc0de63a525b1dc4e
Simon Weis <laguna.ws@gmail.com>
2022-04-10 19:35:35 +0200

    Add test for merging colliders

---
+++ test/integration/performance/tilemap_collisions_performance_test.cpp
@@ -0,0 +1,15 @@
+#include <tilemap/tilemap_collisions.hpp>
+#include <tilemap/tileson_loader.hpp>
+#include <benchmark/benchmark.h>
+
+static void BM_RefineTilemapCollisions(benchmark::State& state)
+{
+    jt::tilemap::TilesonLoader loader { "assets/performance/test_level.json" };
+    auto const collisions = loader.loadCollisionsFromLayer("ground1");
+    for (auto _ : state) {
+        auto newCollisions = collisions;
+        newCollisions.refineColliders(16.0f);
+    }
+}
+
+BENCHMARK(BM_RefineTilemapCollisions)->Unit(benchmark::kMillisecond);
