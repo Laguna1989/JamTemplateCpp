04dab2b07a32bab3c431d1bdb7b4c82409a94da1
Simon Weis <laguna.ws@gmail.com>
2023-11-19 08:42:40 +0100

    Add missing files

---
+++ impl/jamtemplate/common/log/log_target_base.hpp
@@ -0,0 +1,23 @@
+#ifndef JAMTEMPLATE_LOG_TARGET_BASE_HPP
+#define JAMTEMPLATE_LOG_TARGET_BASE_HPP
+
+#include <log/log_target_interface.hpp>
+
+namespace jt {
+
+class LogTargetBase : public jt::LogTargetInterface {
+public:
+    void log(LogEntry const& entry) override;
+
+    void setLogLevel(LogLevel level) override;
+    LogLevel getLogLevel() const;
+
+private:
+    virtual void doLog(LogEntry const& entry) = 0;
+
+    LogLevel m_logLevel { LogLevel::Verbose };
+};
+
+} // namespace jt
+
+#endif // JAMTEMPLATE_LOG_TARGET_BASE_HPP
